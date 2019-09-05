a1ff25297d44480f112adce35724ae75246f5f87
Jeremy <JeremyPlott@gmail.com>
2019-09-05 10:04:44 -0400

    Add project files.

---
+++ CSharpToSQL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharpToSQLLibrary;
+
+namespace CSharpToSQL {
+    class Program {
+
+        void Run() {
+            
+        }
+
+        static void Main(string[] args) {
+            var pgm = new System
+        }
+    }
+}
