f99458bf72fcd259aae78d22e835b3e6e63c2cfc
Jeremy <JeremyPlott@gmail.com>
2019-09-05 14:57:34 -0400

    Added read methods and login check

--- CSharpToSQL/Program.cs
+++ CSharpToSQL/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using CSharpToSQLLibrary;
+using System.Diagnostics;
 
 namespace CSharpToSQL {
 
@@ -9,7 +10,26 @@ namespace CSharpToSQL {
         void Run() {
             var conn = new Connection(@"localhost\sqlexpress", "PRSdb"); //can use 2 backslash to get a single backslash as well. @ = treat each character as a regular character.
             conn.Open();
-            conn.Close();
+            Users.Connection = conn;
+            var logintest = Users.Login("NotAGiraffe", "password");
+            Console.WriteLine(logintest);
+            Console.WriteLine();
+
+            var failedlogin = Users.Login("NotAGiraffe", "asdaa");
+            Console.WriteLine(failedlogin?.ToString() ?? "Not found"); //abbreviated ternary operator, but specifically for nulls.
+            Console.WriteLine();
+
+            var users = Users.GetAll();
+            foreach(var u in users) {
+                Console.WriteLine(u);
+            }
+            Console.WriteLine();
+
+            var user = Users.GetByPK(2);
+            System.Diagnostics.Debug.WriteLine(user); //shows up in the Output tab at the bottom
+
+            var usernf = Users.GetByPK(100);
+            conn.Close(); //add breakpoint here and the users var above will be populated with the user data from SQL
         }
 
         static void Main(string[] args) {
---
+++ CSharpToSQLLibrary/Users.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CSharpToSQLLibrary {
+    public class Users {
+
+        public static Connection Connection { get; set; }
+
+        public static Users Login(string username, string password) {
+            var sql = "SELECT * from users where Username = @Username AND Password = @Password"; 
+            var sqlCmd = new SqlCommand(sql, Connection._Connection);
+            sqlCmd.Parameters.AddWithValue("@Username", username); 
+            sqlCmd.Parameters.AddWithValue("@Password", password); 
+            var reader = sqlCmd.ExecuteReader();
+            if (!reader.HasRows) {
+                reader.Close();
+                return null;
+            }
+            reader.Read();
+            var user = new Users();
+            LoadUserFromSql(user, reader);
+
+            reader.Close();
+            return user;
+        }
+
+        public static Users GetByPK(int id) {
+            var sql = "SELECT * from users where Id = @Id"; //this prevents SQL Injection where as "where Id = " + Id" concatenation is risky
+            var sqlCmd = new SqlCommand(sql, Connection._Connection);
+            sqlCmd.Parameters.AddWithValue("@Id", id); //adds a value to the parameter in the above SQL statement
+            var reader = sqlCmd.ExecuteReader();
+            if(!reader.HasRows) { //if reader.HasRows == false
+                return null;
+            }
+            reader.Read();
+            var user = new Users();
+            LoadUserFromSql(user, reader);
+
+            reader.Close();
+            return user;
+        }
+
+        public static List<Users> GetAll() {
+            var sql = "SELECT * from Users;"; //make sure SQL statements work in SSMS first
+            var sqlCmd = new SqlCommand(sql, Connection._Connection);
+            var reader = sqlCmd.ExecuteReader();
+            var users = new List<Users>();
+            while(reader.Read()) {
+                var user = new Users();
+                users.Add(user);
+                LoadUserFromSql(user, reader);
+            }
+            reader.Close();  //only 1 data reader can be open at a time
+            return users;
+        }
+
+        private static void LoadUserFromSql(Users user, SqlDataReader reader) {
+            user.Id = (int)reader["Id"]; //(int) casts the type to an integer from an object
+            user.Username = reader["Username"].ToString(); //could also use same casting method as above
+            user.Password = reader["Password"].ToString();
+            user.Firstname = reader["Firstname"].ToString();
+            user.Lastname = reader["Lastname"].ToString();
+            user.Phone = reader["Phone"]?.ToString(); //? here = if obj is null, return null, otherwise return obj.ToString()
+            user.Email = reader["Email"]?.ToString();
+            user.IsAdmin = (bool)reader["IsAdmin"];
+            user.IsReviewer = (bool)reader["IsReviewer"];
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool? IsAdmin { get; set; } //? makes it a nullable boolean. Important for nullable columns in SQL.
+        public bool? IsReviewer { get; set; }
+
+        public Users() {
+        }
+
+        public override string ToString() {
+            return $"Id={Id}, Username={Username}, Password={Password}, " +
+                $"Name={Firstname} {Lastname}, Admin?={IsAdmin}, Reviewer?={IsReviewer}";
+        }
+    }
+}
